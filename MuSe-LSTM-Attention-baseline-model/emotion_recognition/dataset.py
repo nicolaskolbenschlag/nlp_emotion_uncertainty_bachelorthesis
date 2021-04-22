fix subjectivity calculation for train partition -> subsequences of limited length treated seperately

MuSe-LSTM-Attention-baseline-model/emotion_recognition/dataset.py
@@ -3,7 +3,6 @@ import torch
 from torch.utils.data.dataset import Dataset
 from torch.nn.utils.rnn import pad_sequence
 
-
 class MyDataset(Dataset):
     def __init__(self, data, partition, subjectivities_per_sample):
         super(MyDataset, self).__init__()
@@ -30,13 +29,22 @@ class MyDataset(Dataset):
         
         # NOTE incorporate subjecities among annotations
         subjectivities = []
-        for meta in metas:
+        for meta in self.metas:
             sample_id = meta[0][0]
+
+            # NOTE for train, samples get split up, so we had to specify the id to distinguish between sub-samples from a video
+            if partition == "train":
+                first_timestamp = meta[0][1]
+                vid_id = f"{sample_id}_{first_timestamp}"
+            else:
+                vid_id = str(vid_id)
+
             subjectivity = torch.tensor(subjectivities_per_sample[sample_id], dtype=torch.float)
             subjectivities += [subjectivity]
         if partition == "train":
             subjectivities = pad_sequence(subjectivities, batch_first=True)
         self.subjectivities = subjectivities
+
 
     def get_feature_dim(self):
         return self.feature_dim

MuSe-LSTM-Attention-baseline-model/emotion_recognition/subjectivity_utilities.py
@@ -9,26 +9,35 @@ def calculate_rolling_subjectivities(params):
     for annotator in range(1,18):#[2,4,5,7,8], range(1,16)
         try:
             data = utils.load_data(params, params.feature_set, params.emo_dim_set, params.normalize, params.label_preproc, params.norm_opts, params.segment_type, params.win_len, params.hop_len, save=params.cache, refresh=params.refresh, add_seg_id=params.add_seg_id, annotator=annotator)
-        except:
+        except Exception as e:
+            print(f"Annotator {annotator}: {e}")
             continue
 
-        for partition in ["devel", "test", "train"]:
+        for partition in ["train", "devel", "test"]:
             metas = data[partition]["meta"]
             labels_ = data[partition]["label"]
 
             assert labels_[0].shape[1] == 1# NOTE currently only one emo dim supported
             for emo_dim in range(labels_[0].shape[1]):
-                # labels = labels_[:,:,emo_dim]
                 labels = [l[:,emo_dim] for l in labels_]
 
-                # NOTE iterate over samples and store them according to their id
+                # NOTE iterate over samples and store them by their id
                 for i, meta in enumerate(metas):
 
+                    # NOTE meta has shape of (seq_len, 3); with 2nd dim: [vid_id,timestamp,segment_id]
                     vid_id = meta[0][0]
-                    timestamps = meta[1]
-                    segment_ids = meta[2]
 
                     label_series = labels[i]
+
+                    # NOTE for training, samples get split up, so if we would not specify id, we would get multiple sub-series per sample and per annotator
+                    if partition == "train":
+                        first_timestamp = meta[0][1]
+                        vid_id = f"{vid_id}_{first_timestamp}"
+                        
+                        if vid_id.split("_")[0] == "85": print(vid_id)
+                        
+                    else:
+                        vid_id = str(vid_id)
 
                     if vid_id not in annotations_per_vid.keys():
                         annotations_per_vid[vid_id] = []
@@ -41,20 +50,30 @@ def calculate_rolling_subjectivities(params):
         subjectivity_of_sample = []
 
         for i, annotation_1 in enumerate(annotations):
-            for j, annotation_2 in enumerate(annotations):
+            for j, annotation_2 in enumerate(annotations[i:]):
                 if i == j:
                     continue
                 
                 # NOTE calculate rolling measuremt of subjectivity between each available annotation
                 rolling_window = 10
-                # subjectivity = pd.Series(annotation_1).rolling(rolling_window).corr(annotation_2)
                 subjectivity = [0.] * (rolling_window - 1)
                 subjectivity += [pd.Series(annotation_1[i - rolling_window : i]).corr(pd.Series(annotation_2[i - rolling_window : i])) for i in range(rolling_window, len(annotation_1) + 1)]
-                
+                subjectivity = pd.Series(subjectivity).fillna(.0)# NOTE [0,0,0].corr([0,0,0]) = nan
                 subjectivity_of_sample += [subjectivity]
         
+        ################################
         assert len(subjectivity_of_sample) >= 1, f"too less annotations for sample {vid_id}"
-        print(f"vid{vid_id}: {[(len(s) for s in subjectivity_of_sample)]}")
+        # err = False
+        # for a in subjectivity_of_sample:
+        #     if len(a) != len(subjectivity_of_sample[0]):
+        #         err = True
+        #         break
+        # if err:
+        #     print(f"vid{vid_id}: {len(annotations)} - 1st: {len(annotations[0])}")
+        #     # print(f"vid{vid_id}: {subjectivity_of_sample[0]}")
+        #     tmp = [len(s) for s in subjectivity_of_sample]
+        #     print(f"vid{vid_id}: {tmp}")
+        # ################################
         
         # NOTE calculate element-wise mean to get average subjectivity at each timestep
         subjectivity_of_sample = np.stack(subjectivity_of_sample).mean(axis=0)