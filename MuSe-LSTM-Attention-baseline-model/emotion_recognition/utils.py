first implementation of new tiltedCCC + fix syntax error in call of pebUME + add prints in uncertainty metrics calculation to debug very long execution time

MuSe-LSTM-Attention-baseline-model/emotion_recognition/utils.py
@@ -521,7 +521,7 @@ class TiltedCCCLoss(nn.Module):
         loss = torch.mean(torch.cat(losses))
         return loss
     
-    def forward(self, y_pred, y_true, seq_lens=None, label_smooth=None):
+    def forward_assignment_by_sign(self, y_pred, y_true, seq_lens=None, label_smooth=None):
         """
         :param y_pred: (batch_size, seq_len)
         :param y_true: (batch_size, seq_len)
@@ -576,6 +576,37 @@ class TiltedCCCLoss(nn.Module):
         # ccc = ccc.squeeze(0)  # (*,) if necessary
         ccc_loss = 1.0 - ccc
         return ccc_loss
+    
+    def forward(self, y_pred, y_true, seq_lens=None, label_smooth=None):
+        """
+        :param y_pred: (batch_size, seq_len)
+        :param y_true: (batch_size, seq_len)
+        :param seq_lens: (batch_size,)
+        :return:
+        """
+        # make padding mask
+        if seq_lens is not None:
+            mask = torch.ones_like(y_true, device=y_true.device)
+            for i, seq_len in enumerate(seq_lens): mask[i, seq_len:] = 0
+        else:
+            mask = torch.ones_like(y_true, device=y_true.device)
+
+        mean_prediction_error = TiltedCCCLoss.compute_ccc(y_pred[:,:,0], y_true, mask)
+        losses = [mean_prediction_error]
+        import uncertainty_utilities
+        # NOTE unlimited number of window sizes would be possible
+        windows = [10,100]
+        for i, window in enumerate(windows, 1):
+            rolling_correlation = torch.tensor([uncertainty_utilities.rolling_correlation_coefficient(yt, yp[:,i], window) for yt, yp in zip(y_true, y_pred)], dtype="float")
+            # NOTE this acts as tilt; as larger the error, as larger the influence on this quantile's error (so this 'quantile' willl focus on avoiding 'window'-term-errors)
+            rolling_correlation_error = (1. - rolling_correlation) / 2
+            # NOTE calculate actual loss
+            ccc_loss = TiltedCCCLoss.compute_ccc(y_pred[:,:,i], y_true, mask)
+            # NOTE perform the tilt (note: ccc is one number per whole sample, while rolling correlation is one value per timesteps; so take mean to get to whole sample)
+            ccc_loss = ccc_loss * rolling_correlation_error.mean(dim=-1)
+            losses += [ccc_loss.mean()]
+        loss = torch.mean(torch.cat(losses))
+        return loss
 
 
 class CCCLoss(nn.Module):