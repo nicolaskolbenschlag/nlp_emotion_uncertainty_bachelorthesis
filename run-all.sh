293ded789a8361ec6cbc74e123fe606323042f09
Nicolas Kolbenschlag <nicolas.kolbenschlag@googlemail.com>
2021-05-24 10:23:25 +0200

    organize slurm script

--- run-all.sh
+++ run-all.sh
@@ -11,19 +11,15 @@ source $START_HERE/venv/bin/activate
 features="bert-4"
 emo_dim="valence"
 
-
 # MONTE CARLO DROPOUT   
-# cmd="$START_HERE/MuSe-LSTM-Attention-baseline-model/emotion_recognition/main_uncertainty.py --feature_set $features --emo_dim_set $emo_dim --epochs 100 --refresh --n_seeds 3 --seed 314 --attn --rnn_bi --loss ccc --uncertainty_approach monte_carlo_dropout --attn_dr .5 --out_dr .5 --rnn_n_layers 2 --rnn_dr .5"
-cmd="$START_HERE/MuSe-LSTM-Attention-baseline-model/emotion_recognition/main_uncertainty.py --feature_set $features --emo_dim_set $emo_dim --epochs 1 --refresh --n_seeds 1 --seed 314 --attn --rnn_bi --loss ccc --uncertainty_approach monte_carlo_dropout --attn_dr .5 --out_dr .5 --rnn_n_layers 2 --rnn_dr .5"
-python3 $cmd
-
-# RANDOM UNCERTAINTY QUANTIFICATIONS
-cmd="$START_HERE/MuSe-LSTM-Attention-baseline-model/emotion_recognition/main_uncertainty.py --feature_set $features --emo_dim_set $emo_dim --epochs 100 --refresh --n_seeds 3 --seed 314 --attn --rnn_bi --loss ccc"
-# python3 $cmd
+# python3 "$START_HERE/MuSe-LSTM-Attention-baseline-model/emotion_recognition/main_uncertainty.py --feature_set $features --emo_dim_set $emo_dim --epochs 100 --refresh --n_seeds 3 --seed 314 --attn --rnn_bi --loss ccc --uncertainty_approach monte_carlo_dropout --attn_dr .5 --out_dr .5 --rnn_n_layers 2 --rnn_dr .5"
+python3 "$START_HERE/MuSe-LSTM-Attention-baseline-model/emotion_recognition/main_uncertainty.py --feature_set $features --emo_dim_set $emo_dim --epochs 1 --refresh --n_seeds 1 --seed 314 --attn --rnn_bi --loss ccc --uncertainty_approach monte_carlo_dropout --attn_dr .5 --out_dr .5 --rnn_n_layers 2 --rnn_dr .5"
 
 # TILTED-CCC
-cmd="MuSe-LSTM-Attention-baseline-model/emotion_recognition/main_uncertainty.py --feature_set $features --emo_dim_set $emo_dim --epochs 100 --refresh --n_seeds 3 --seed 314 --attn --rnn_bi --uncertainty_approach quantile_regression --loss tiltedCCC"
-# python3 $cmd
+# python3 "$START_HERE/MuSe-LSTM-Attention-baseline-model/emotion_recognition/main_uncertainty.py --feature_set $features --emo_dim_set $emo_dim --epochs 100 --refresh --n_seeds 3 --seed 314 --attn --rnn_bi --uncertainty_approach quantile_regression --loss tiltedCCC"
+
+# RANDOM UNCERTAINTY QUANTIFICATIONS
+# python3 "$START_HERE/MuSe-LSTM-Attention-baseline-model/emotion_recognition/main_uncertainty.py --feature_set $features --emo_dim_set $emo_dim --epochs 100 --refresh --n_seeds 3 --seed 314 --attn --rnn_bi --loss ccc"
 
 # PREDICT SUBJECTIVITY
-cmd="MuSe-LSTM-Attention-baseline-model/emotion_recognition/main_uncertainty.py --feature_set $features --emo_dim_set $emo_dim --epochs 100 --refresh --n_seeds 3 --seed 314 --attn --rnn_bi --loss ccc --not_measure_uncertainty --predict_subjectivity"
+# python3 "$START_HERE/MuSe-LSTM-Attention-baseline-model/emotion_recognition/main_uncertainty.py --feature_set $features --emo_dim_set $emo_dim --epochs 100 --refresh --n_seeds 3 --seed 314 --attn --rnn_bi --loss ccc --not_measure_uncertainty --predict_subjectivity"
